5bf6bb2d6733e7c58b4e4eb8285071646b03d979
Vasilis Kiouras <msn-1010@live.com>
2012-03-23 02:59:44 +0200

    updated Vslackpkg.c to current elm, splitted gfx

--- Vslackpkg.c
+++ Vslackpkg.c
@@ -1,118 +1,93 @@
-// Compile with: 
-// gcc Vpkg.c -o Vpkg -Wall `pkg-config --cflags --libs elementary`
+
+// compile with :
+// gcc vslackpkg.c -o vslackpkg -Wall `pkg-config --cflags --libs elementary`
 
 #include <Elementary.h>
 #ifdef HAVE_CONFIG_H
-# include "elementary_config.h"
+#include "elementary_config.h"
 #else
-# define __UNUSED__
+#define __UNUSED__
 #endif
 
-typedef struct
-{
-	Eina_List *transitions;
-	Evas_Object *box;
-	Evas_Object_Box_Layout last_layout;
-}
 
-Transitions_Data;
+#include "graphics.h"
+
+
 static void
-_clear_cb(void *data, Evas_Object *obj __UNUSED__, void *ev __UNUSED__)
+_on_done(void *data __UNUSED__,
+		Evas_Object *obj __UNUSED__,
+		void *event_info __UNUSED__)
 {
- Transitions_Data *tdata = data;
- elm_box_clear(tdata->box);
+	elm_exit();
 }
 
 
-int
-elm_main(int argc __UNUSED__, char *argv[] __UNUSED__)
+static void
+_item_1_pressed(void *data, Evas_Object *obj __UNUSED__, void *event_info __UNUSED__)
 {
-   Evas_Object *win, *bg, *bigbox, *bx, *bt;
-   static Transitions_Data tdata = {
-        .transitions = NULL,
-        .box = NULL,
-        .last_layout = NULL
-   };
-
-   elm_policy_set(ELM_POLICY_QUIT, ELM_POLICY_QUIT_LAST_WINDOW_CLOSED);
+   Evas_Object *bt = data;
+   elm_object_text_set(bt, "Install");
+}
 
 
-// Create a new window with name and title
-   win = elm_win_add(NULL, "Vpkg1", ELM_WIN_BASIC);
-   elm_win_title_set(win, "Vpkg1");
-   elm_win_autodel_set(win, EINA_TRUE);
-   
-   
-// Background
-   bg = elm_bg_add(win);
-   elm_win_resize_object_add(win, bg);
-   evas_object_size_hint_weight_set(bg, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
-   evas_object_show(bg);
-   
-   
-// Box object
-   bigbox = elm_box_add(win);
-   evas_object_size_hint_weight_set(bigbox, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
-   elm_win_resize_object_add(win, bigbox);
-   evas_object_show(bigbox);
-   
-   
-// Box object
-   bx = elm_box_add(win);
-   elm_box_horizontal_set(bx, EINA_TRUE);
-   elm_box_pack_end(bigbox, bx);
-   evas_object_show(bx);
-   
-   
-// Add Install button
-   bt = elm_button_add(win);
-   elm_button_label_set(bt, "Install");
-   elm_box_pack_end(bx, bt);
-   evas_object_show(bt);
-   
-   
-// Add Remove button
-   bt = elm_button_add(win);
-   elm_button_label_set(bt, "Remove");
-   elm_box_pack_end(bx, bt);
-   evas_object_show(bt);
-   evas_object_smart_callback_add(bt, "clicked", _clear_cb, &tdata);
-   
-   
-// Box object
-   bx = elm_box_add(win);
-   elm_box_horizontal_set(bx, EINA_TRUE);
-   evas_object_size_hint_weight_set(bx, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
-   evas_object_size_hint_align_set(bx, EVAS_HINT_FILL, EVAS_HINT_FILL);
-   elm_box_pack_end(bigbox, bx);
-   evas_object_show(bx);
-   
-	 
-// Add Ok button
-   bt = elm_button_add(win);
-   elm_button_label_set(bt, "Ok");
-   elm_box_pack_end(bx, bt);
-   evas_object_show(bt);
-   
-   
-// Add Cancel button
-   bt = elm_button_add(win);
-   elm_button_label_set(bt, "Cancel");
-   elm_box_pack_end(bx, bt);
-   evas_object_show(bt);
-   
-   
-// Show the size of window
-   evas_object_resize(win, 400, 420);
+static void
+_item_2_pressed(void *data, Evas_Object *obj __UNUSED__, void *event_info __UNUSED__)
+{
+   Evas_Object *bt = data;
+   elm_object_text_set(bt, "Remove");
+}
 
 
-// Show the window
-   evas_object_show(win);
-   
+static void
+_item_3_pressed(void *data, Evas_Object *obj __UNUSED__, void *event_info __UNUSED__)
+{
+   Evas_Object *bt = data;
+   elm_object_text_set(bt, "About");
+}
+
+
+static void
+list_install(void *data, Evas_Object *obj __UNUSED__, void *event_info __UNUSED__)
+{
+	Evas_Object *li = data;
+	elm_list_clear(li);
+	Eina_List *files ;
+	Eina_List *l;
+	char *package;
+	files = ecore_file_ls("/var/log/packages/");
+	EINA_LIST_FOREACH(files, l , package)
+	elm_list_item_append(li,package, NULL, NULL, NULL, NULL);
+	eina_list_free(files);	
+}
+
+
+static void
+list_remove(void *data, Evas_Object *obj __UNUSED__, void *event_info __UNUSED__)
+{
+	Evas_Object *li = data;
+	elm_list_clear(li);
+	Eina_List *files ;
+	Eina_List *l;
+	char *package;
+	files = ecore_file_ls("/var/log/packages/");
+	EINA_LIST_FOREACH(files, l , package)
+	elm_list_item_append(li,package, NULL, NULL, NULL, NULL);
+	eina_list_free(files);	
+}
+
+
+EAPI_MAIN int
+elm_main(int argc __UNUSED__, char **argv __UNUSED__)
+{
+
+   vslackpkg V; 
+   init (&V);
+
+   evas_object_smart_callback_add(V.win, "delete,request", _on_done, NULL);
 
    elm_run();
-   elm_shutdown();
    return 0;
-
 }
-ELM_MAIN();
+
+ELM_MAIN()
+
---
+++ graphics.h
@@ -0,0 +1,106 @@
+
+
+
+typedef struct vslackpkg{
+ Evas_Object *win; 
+ Evas_Object *bg;
+ Evas_Object *bigbox;
+ Evas_Object *smallbox; 
+ Evas_Object *tb; //toolbar
+ Evas_Object *bt_ok; //button ok
+ Evas_Object *bt_cancel; //button cancel
+ Evas_Object *li;//list
+} vslackpkg;
+
+
+void init (vslackpkg *X){
+
+// create a new window   
+
+   X->win = elm_win_add(NULL, "vslack_pkg", ELM_WIN_BASIC);
+   elm_win_title_set(X->win, "vslack_pkg");
+
+
+// add a standard background  
+
+   X->bg = elm_bg_add(X->win);
+   elm_win_resize_object_add(X->win, X->bg);
+   evas_object_size_hint_weight_set(X->bg, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+   evas_object_show(X->bg);
+
+
+// add a bigbox object   
+
+   X->bigbox = elm_box_add(X->win);
+   elm_win_resize_object_add(X->win, X->bigbox);
+   evas_object_size_hint_weight_set(X->bigbox, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+   evas_object_size_hint_align_set(X->bigbox, EVAS_HINT_FILL, EVAS_HINT_FILL);
+   evas_object_show(X->bigbox);
+
+
+//add a toolbar object with buttons   
+
+   X->tb = elm_toolbar_add(X->win);
+   elm_toolbar_shrink_mode_set(X->tb, ELM_TOOLBAR_SHRINK_SCROLL);
+   evas_object_size_hint_weight_set(X->tb, 0.0, 0.0);
+   evas_object_size_hint_align_set(X->tb, EVAS_HINT_FILL, 0.0);
+   evas_object_show(X->tb);
+
+  
+   elm_toolbar_homogeneous_set(X->tb, EINA_FALSE);
+  
+   elm_box_pack_end(X->bigbox, X->tb);
+   //elm_box_pack_end(X->bigbox, X->bt);
+
+
+   X->li = elm_list_add(X->win);
+   evas_object_size_hint_weight_set(X->li, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+   evas_object_size_hint_align_set(X->li, EVAS_HINT_FILL, EVAS_HINT_FILL);
+   elm_box_pack_end(X->bigbox, X->li);
+   elm_list_item_append(X->li, "Remove", NULL, NULL, NULL, NULL);
+   evas_object_show(X->li);
+
+   
+   elm_toolbar_item_append(X->tb, NULL, "Install", NULL, X->li);
+   elm_toolbar_item_append(X->tb, NULL, "Remove", NULL , X->li);
+   elm_toolbar_item_append(X->tb, NULL, "About", NULL, NULL);
+  
+
+//add horizontal smallbox inside bigbox
+
+   X->smallbox = elm_box_add(X->win);
+   elm_box_horizontal_set(X->smallbox, EINA_TRUE);
+   evas_object_size_hint_weight_set(X->smallbox, EVAS_HINT_EXPAND, -0.2);
+   evas_object_size_hint_align_set(X->smallbox, EVAS_HINT_FILL, EVAS_HINT_FILL);
+   elm_win_resize_object_add(X->win, X->smallbox);
+   elm_box_pack_end(X->bigbox, X->smallbox);
+   evas_object_show(X->smallbox);
+
+
+//add ok button to smallbox
+
+   X->bt_ok = elm_button_add(X->win);
+   elm_object_text_set(X->bt_ok, "Ok");
+   evas_object_size_hint_weight_set(X->bt_ok, EVAS_HINT_EXPAND, 0);
+   evas_object_size_hint_align_set(X->bt_ok, EVAS_HINT_FILL, 0);
+   elm_box_pack_end(X->smallbox, X->bt_ok);
+   evas_object_show(X->bt_ok);
+
+
+//add cancel button to smallbox
+
+   X->bt_cancel = elm_button_add(X->win);
+   elm_object_text_set(X->bt_cancel, "Cancel");
+   evas_object_size_hint_weight_set(X->bt_cancel, EVAS_HINT_EXPAND, 0);
+   evas_object_size_hint_align_set(X->bt_cancel, EVAS_HINT_FILL, 0);
+   elm_box_pack_end(X->smallbox, X->bt_cancel);
+   evas_object_show(X->bt_cancel);
+
+   
+   elm_list_go(X->li);
+
+   evas_object_resize(X->win, 650, 520);   
+   evas_object_show(X->win);
+} 
+
+
